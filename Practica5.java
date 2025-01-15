584ef69d01ad704261a13c4fa027be66a30361fe
giselooo <145780354+giselooo@users.noreply.github.com>
2025-01-14 21:05:50 -0600

    Practica5.java

--- Practica5.java
+++ Practica5.java
@@ -1,18 +1,20 @@
+import java.util.List;
+import java.util.ArrayList;
 
 class Solution {
     List<Boolean> kidsWithCandies(List<Integer> candies, int extraCandies) {
         List<Boolean> result = new ArrayList<>();
         int greatest = 0;
-        for(int i = 0; i<candies.size();i++){
-            if(greatest=candies.get(i))
-            }
-        }
-
-       for(int i = 0; i<candies.size();i++){
+        for(int i = 0; i<candies.size(); i++){
+            if(greatest<=candies.get(i)){
+            } 
+        }     
+       
+       for(int i = 0; i<candies.size(); i++){
         result.add(candies.get(i) + extraCandies >= greatest);
-
+        
        }
-
+       
        return result;
-      
-    }
+}
+}
